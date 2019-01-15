5798be80ed0877f6c5489564902598d30ebcad4c
Isaac Dorfman <isaac.i.dorfman@gmail.com>
2019-01-15 14:24:27 +0200

    Handled file loading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Returns an array of distances from pacman to the ghosts')
 parser.add_argument("--board", help="Path to the file containing the board")
@@ -7,3 +8,12 @@ args = parser.parse_args()
 if not args.board:
     print("No board path specified")
     exit(-1)
+
+try:
+    board = np.load(args.board)
+except:
+    print("Could not load board")
+    exit(-1)
+
+
+
