3522ac6988c57022466f1fbc9a2794d897d2ca23
Isaac Dorfman <isaac.i.dorfman@gmail.com>
2019-01-15 15:10:04 +0200

    Added a function to calculate distances in board

--- main.py
+++ main.py
@@ -23,3 +23,27 @@ def getGhostIndices(board):
     temp = np.where(board == 2)
     return zip(board[0],board[1])
 
+boardSize = board.shape[0] * board.shape[1]
+distancesBoard = (boardSize + 1) * np.ones(board.shape,board.dtype)
+
+def calculateDistances(x,y, currentDistance):
+    if x < 0 or x >= board.shape[0]:
+        return
+    if y < 0 or y >= board.shape[1]:
+        return
+    if board[x,y] == 1:
+        return
+    if currentDistance >= distancesBoard[x,y]:
+        return
+    distancesBoard[x,y] = currentDistance
+    calculateDistances(x-1,y,currentDistance + 1)
+    calculateDistances(x,y-1,currentDistance + 1)
+    calculateDistances(x+1,y,currentDistance + 1)
+    calculateDistances(x,y+1,currentDistance + 1)
+
+pacmanIndex = getPacmanIndex(board)
+calculateDistances(pacmanIndex[0], pacmanIndex[1], 0)
+
+ghosts = getGhostIndices(board)
+print(ghosts)
+
